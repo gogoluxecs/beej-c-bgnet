98a6602098af7bb739307a4f12bc92c3bc49e2cb
gogoluxecs <georgi.lambov@gmail.com>
2014-05-30 17:21:56 +0300

    Scoket server with stream socket.

---
+++ Server/server.c
@@ -0,0 +1,78 @@
+// simple server
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490"
+#define BACKLOG 10
+
+void sigchld_handler(int a) {
+	while(waitpid(-1, NULL, WNOHANG) > 0 );
+}
+
+void *get_in_addr(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void) {
+	int sockfd, new_fd; // listen on sock_fs, new connection on new_fd
+	struct addrinfo hints, *serverinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my ip
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &serverinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for (p = serverinfo; p != NULL; p = p->ai_next) {
+		if((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+			sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+}
